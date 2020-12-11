b91ee85dd09fe59e33c72d4ae49422743559475c
adroit-akash <akashctd03@gmail.com>
2020-12-11 19:59:53 +0530

    Revert "first commit"
    This reverts commit ac8d3783a07e7edc67eeaed5fcb9ffae36d3720e.

--- src/components/search/search.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./search.css";
-
-export const Search=({placeholder,onchange})=>(
-    <input className='search' type='search' placeholder={placeholder}
-    onChange={onchange}></input>
-)
-        // <input placeholder='search monster' onChange={(e)=>(this.setState({search:e.target.value}))}></input>
